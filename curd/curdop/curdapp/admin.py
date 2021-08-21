beea9d0b78eb77f906922f22338b32e4e38244c2
k-nareshkumar <89312244+k-nareshkumar@users.noreply.github.com>
2021-08-21 21:46:25 +0530

    Add files via upload

---
+++ curd/curdop/curdapp/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from curdapp import models
+from .models import student
+admin.site.register(student)
+
+# Register your models here.
